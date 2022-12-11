c8cedd3ea3077bccf9712e164df57b8413284a79
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2022-12-11 23:18:23 +0400

    Add project files.

---
+++ ConsoleAppYndhanur/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleAppYndhanur;
+
+public class Program
+{
+    public static void Main(string[] args)//Cragri himnakan masna vortegh sksvum u prtsnuma,linuma mi qani tesak
+    {
+        StaticMain Esi = new StaticMain();
+        Esi.a = 8;
+        Console.WriteLine(Esi.a);//Debug ov nayeluc parza linum vor staticy hasaneliya clasi bolor andamnerin 
+                                 //aranc obj haytararelu linuma et metodi kam tari het ashxatel menak class+dot ov
+    }
+}
---
+++ ConsoleAppYndhanur/StaticMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppYndhanur
+{
+    public class StaticMain
+    {
+        public int a =5;
+        public static int b = 8;
+        static void MyStat(int n)
+        {
+            Console.WriteLine(n);
+        }
+        public int MyStat2(int g)
+        {
+            return g + 5;
+        }
+    }
+}
